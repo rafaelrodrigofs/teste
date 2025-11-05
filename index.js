72d77f38f5312427e5c7ab179e9abe94b271fc6f
Rafael Rodrigo <rafaelrodrigoartes@gmail.com>
2025-11-05 08:23:23 -0300

    Sera que é a porta ?

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on port ${port}`);
+});
